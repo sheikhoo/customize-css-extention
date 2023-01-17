bcb5d8dc32b3c9e4f4ebf9ed1f7556c6afbe43ac
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2023-01-17 09:54:56 +0330

    fix insert css to site

--- js/popup.js
+++ js/popup.js
@@ -2,15 +2,19 @@
 const beautify = window.js_beautify.css;
 
 let customCSS;
-let fileName = 'custom.css';
-let fileUrl;
 
-// Retrieve custom CSS from local storage using the current tab URL as the key
+// Retrieve custom CSS from storage using the current tab URL as the key
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  customCSS = localStorage.getItem(tabs[0].url);
-  // Insert custom CSS into the current website
-  chrome.tabs.executeScript(tabs[0].id, {
-    code: 'document.body.style.cssText += "' + customCSS + '";',
+  chrome.storage.local.get(tabs[0].url, function (result) {
+    customCSS = result[tabs[0].url];
+    // Insert custom CSS into the current website
+    chrome.tabs.executeScript(tabs[0].id, {
+      code:
+        "var style = document.createElement('style'); style.innerHTML = '" +
+        customCSS.replace(/(\r\n|\n|\r)/gm, '') +
+        "'; document.head.appendChild(style);",
+    });
+    document.getElementById('custom-css').value = customCSS;
   });
 });
 
@@ -18,11 +22,16 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   // Check if the tab is fully loaded
   if (changeInfo.status === 'complete') {
-    // Retrieve custom CSS from local storage using the current tab URL as the key
-    customCSS = localStorage.getItem(tab.url);
-    // Insert custom CSS into the website
-    chrome.tabs.executeScript(tabId, {
-      code: 'document.body.style.cssText += "' + customCSS + '";',
+    // Retrieve custom CSS from storage using the current tab URL as the key
+    chrome.storage.local.get(tab.url, function (result) {
+      customCSS = result[tab.url];
+      // Insert custom CSS into the website
+      chrome.tabs.executeScript(tabs[0].id, {
+        code:
+          "var style = document.createElement('style'); style.innerHTML = '" +
+          customCSS.replace(/(\r\n|\n|\r)/gm, '') +
+          "'; document.head.appendChild(style);",
+      });
     });
   }
 });
@@ -32,39 +41,27 @@ document.getElementById('custom-css').value = customCSS;
 
 // Save button click event
 document.getElementById('save-button').addEventListener('click', function () {
-  alert('save');
   // Get the custom CSS from the textarea
   customCSS = document.getElementById('custom-css').value;
-  // Save the custom CSS to file
-  chrome.fileSystem.chooseEntry(
-    { type: 'openWritableFile', suggestedName: fileName },
-    function (writableFileEntry) {
-      writableFileEntry.createWriter(
-        function (writer) {
-          writer.onwriteend = function (e) {
-            fileUrl = writer.toURL();
-          };
-          writer.write(new Blob([customCSS], { type: 'text/css' }));
-        },
-        function (e) {
-          console.log(e);
-        }
-      );
-    }
-  );
-  // Save the custom CSS to local storage using the current tab URL as the key
+  // Save the custom CSS to storage using the current tab URL as the key
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    localStorage.setItem(tabs[0].url, customCSS);
-    // Insert the custom CSS into the current website
-    chrome.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.cssText += "' + customCSS + '";',
+    let data = {};
+    data[tabs[0].url] = customCSS;
+    chrome.storage.local.set(data, function () {
+      // Insert the custom CSS into the current website
+      chrome.tabs.executeScript(tabs[0].id, {
+        code:
+          "var style = document.createElement('style'); style.innerHTML = '" +
+          customCSS.replace(/(\r\n|\n|\r)/gm, '') +
+          "'; document.head.appendChild(style);",
+      });
     });
   });
-
-  document
-    .getElementById('beautify-button')
-    .addEventListener('click', function () {
-      let css = document.getElementById('custom-css').value;
-      document.getElementById('custom-css').value = beautify(css);
-    });
 });
+
+document
+  .getElementById('beautify-button')
+  .addEventListener('click', function () {
+    let css = document.getElementById('custom-css').value;
+    document.getElementById('custom-css').value = beautify(css);
+  });
